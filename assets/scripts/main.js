2deeae83cb4e810464b8184adeccb8ce5f743b9a
wis003 <wis003@ucsd.edu>
2021-02-05 15:52:23 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,34 +1,28 @@
 // main.js
-let sound = new Audio();
-let image = new Image();
-document.getElementById("radio-air-horn").addEventListener("checked", changeSoundHorn);
-document.getElementById("radio-car-horn").addEventListener("checked", changeSoundCar);
-document.getElementById("radio-party-horn").addEventListener("checked", changeSoundParty);
+
+document.getElementById("radio-air-horn").addEventListener("change", changeSoundHorn);
+document.getElementById("radio-car-horn").addEventListener("change", changeSoundCar);
+document.getElementById("radio-party-horn").addEventListener("change", changeSoundParty);
 
 function changeSoundHorn() {
-    // document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
-    // document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
-    // document.getElementById("sound-image").alt = "Air Horn";
-    sound.src = "./assets/media/audio/air-horn.mp3";
-
+    document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
+    document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+    document.getElementById("sound-image").alt = "Air Horn";
 }
 function changeSoundCar() {
-    // document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
-    // document.getElementById("sound-image").src = "./assets/media/images/car.svg";
-    // document.getElementById("sound-image").alt = "Car Horn";
-    sound.src = "./assets/media/audio/car-horn.mp3";
-
+    document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
+    document.getElementById("sound-image").src = "./assets/media/images/car.svg";
+    document.getElementById("sound-image").alt = "Car Horn";
 }
 function changeSoundParty() {
-    // document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
-    // document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
-    // document.getElementById("sound-image").alt = "Party Horn";
-    sound.src = "./assets/media/audio/party-horn.mp3";
+    document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
+    document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+    document.getElementById("sound-image").alt = "Party Horn";
 }
 
 document.getElementById("honk-btn").addEventListener("click", playSound);
 
-function playSound() {
-    // document.getElementById("honk-btn").src = document.getElementById("horn-sound").src;
-    sound.play();
+function playSound(event) {
+    document.getElementById("horn-sound").play();
+    event.preventDefault();
 }
