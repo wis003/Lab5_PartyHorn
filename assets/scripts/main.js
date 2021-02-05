4e0026893cd129ca41a0ce7f62a7ddf183851c0c
wis003 <wis003@ucsd.edu>
2021-02-05 14:54:39 -0800

    Part 1 Sounds

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,26 @@
 // main.js
+document.getElementById("radio-air-horn").addEventListener("checked", changeSoundHorn);
+document.getElementById("radio-car-horn").addEventListener("checked", changeSoundCar);
+document.getElementById("radio-party-horn").addEventListener("checked", changeSoundParty);
 
-// TODO
+function changeSoundHorn() {
+    document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
+    document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+    document.getElementById("sound-image").alt = "Air Horn";
+}
+function changeSoundCar() {
+    document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
+    document.getElementById("sound-image").src = "./assets/media/images/car.svg";
+    document.getElementById("sound-image").alt = "Car Horn";
+}
+function changeSoundParty() {
+    document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
+    document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+    document.getElementById("sound-image").alt = "Party Horn";
+}
+
+document.getElementById("honk-btn").addEventListener("click", playSound);
+
+function playSound() {
+    document.getElementById("honk-btn").src = document.getElementById("horn-sound").src;
+}
